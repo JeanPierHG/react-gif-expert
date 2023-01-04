c93d7bfecf51f2df07349b70686978803599cdc0
JeanPierHG <jeanpier.dev@outlook.com>
2023-01-04 18:15:46 -0500

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,9 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=dLZWowRTePxy0A8VETa5yc41N4Y8XXdH&q=${category}&limit=10`;
+
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  const gifs = data.map((img) => ({ id: img.id, title: img.title, url: img.images.downsized_medium.url }));
+  console.log({ gifs });
+  return gifs;
+};
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import { getGifs } from '../helpers/getGifs';
+
+export const useFetchGifs = (category) => {
+  const [gifs, setGifs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    getGifs(category).then((newGifs) => {
+      setGifs(newGifs);
+      setIsLoading(false);
+    });
+  }, []);
+  return { gifs, isLoading };
+};
